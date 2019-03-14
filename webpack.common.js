3ecb4414525b9ae51a0e09a642421be68aa879da
jtans <jtans@qq.com>
2019-03-14 15:12:38 +0800

    auto multi entry

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+var utils = require('./config');
+var glob = require('glob')
+//当前工作目录的绝对路径
+var ROOT_PATH = path.resolve(__dirname);
+//要打包的源代码路径
+var RESOURCES_PATH = path.resolve(ROOT_PATH, 'src');
+//设置要打包的js文件为入口文件
+var entrys = utils.getEntries(path.join(RESOURCES_PATH, '/*.js'), path.join(RESOURCES_PATH, '/'));
 
 module.exports = {
-  entry: {
-    11111: './src/index.js'
-    // print: './src/print.js'
-  },
+  entry: entrys,
   devServer: {
       contentBase: './dist'
   },
@@ -16,4 +21,4 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
-};
+}
